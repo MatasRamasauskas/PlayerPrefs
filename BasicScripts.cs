c362a3512539b6c4666cff18419d5897c08db8ba
Matas <dev.matas@gmail.com>
2015-06-12 15:43:33 +0300

    Commitas

---
+++ BasicScripts.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BasicScripts : MonoBehaviour {
+
+
+    public Slider soundSlider;
+    private int soundSliderValue;
+
+	// Use this for initialization
+	void Start () 
+    {
+        soundSlider.value = PlayerPrefs.GetInt("SoundSlider");
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if(soundSlider.value == 0)
+        {
+            soundSliderValue = 0;
+            PlayerPrefs.SetInt("SoundSlider", soundSliderValue);
+        }
+        if (soundSlider.value == 1)
+        {
+            soundSliderValue = 1;
+            PlayerPrefs.SetInt("SoundSlider", soundSliderValue);
+        }
+	
+	}
+}
